d572f2d63258afcf526ceda5d67dec286fa8f66c
derekj.williams <247fear@gmail.com>
2017-07-05 20:11:29 -0500

    Product made

---
+++ db/migrate/20170706011052_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :image
+      t.string :type
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
