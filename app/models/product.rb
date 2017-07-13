e4dccca341933af5a7bccf1451cba858cc3a071b
derekj.williams <247fear@gmail.com>
2017-07-12 21:01:16 -0500

    sale_message added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+  def sale_message
+    if price.to_i < 340000 
+      return "Low priced dragon. May be harder to train."
+    else
+      return "High priced dragon. Tends to be easier to train."
+    end
+
+    def tax
+      price * 0.009
+    end
+  end
 end
