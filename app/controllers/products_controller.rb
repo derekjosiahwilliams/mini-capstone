22e581c31f2bc060344001e102af5328168b23ea
derekj.williams <247fear@gmail.com>
2017-07-12 20:12:06 -0500

    Looks nice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,48 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "index.html.erb"
   end
+
+  def show
+    dragons_id = params[:id]
+    @products = Product.find_by(id: dragons_id)
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.new(name: params[:form_name], price: params[:form_price], element: params[:form_element], description: params[:form_description], image: params[:form_image])
+    product.save
+    flash[:success] = "A Dragon has been born!"
+    redirect_to "/dragons/#{product.id}"
+  end
+
+  def edit
+    dragons_id = params[:id]
+    @product = Product.find_by(id: dragons_id)
+    product.save
+    render "edit.html.erb"
+  end
+
+  def update
+    dragons_id = params[:id]
+    @product = Product.find_by(id: dragons_id)
+    @product.name = params[:form_name]
+    @product.price = params[:form_price]
+    @product.element = params[:form_element]
+    @product.description = params[:form_description]
+    @product.save
+    flash[:success] = "Dragon Improved!"
+    redirect_to "/dragons/#{@dragons.id}"
+  end
+
+  def destory
+    dragons_id = params[:id]
+    @product = Product.find_by(id: dragons_id)
+    @product.destroy
+    render "destory.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "products#index"
+
   get "/dragons" => "products#index"
+  get "/dragons/new" => "products#new"
+  post "/dragons" => "products#create"
+  get "/dragons/:id" => "products#show"
+  get "/dragons/:id/edit" => "products#edit"
+  patch "/dragons/:id" => "products#update"
+  delete "dragons/:id" => "products#destory"
 end
